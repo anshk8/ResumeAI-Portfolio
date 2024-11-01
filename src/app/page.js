02c695aaa26b2e986f7af218be7bac8b1c94847f
Ansh <anshkakkar05@gmail.com>
2024-11-01 15:49:06 -0400

    Error Message

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,17 @@ import MovingText from './components/MovingText'
 import Footer from './components/Footer'
 import styles from './globals.css'
 
+import Head from 'next/head'; // Import Head from next/head
+
 export default function Home() {
   return (
     <div className="App">
+         <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Your Site Title</title>
+        <meta name="description" content="A brief description of your site." />
+        {/* Add other meta tags as needed */}
+      </Head>
     <NavBar />
 
    <ChatBox />
