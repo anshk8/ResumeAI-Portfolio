3ca05da0dfd079774c03986d287e2ea5e9f459d9
Ansh <anshkakkar05@gmail.com>
2024-11-01 12:00:35 -0400

    Removed react-typed

--- src/app/components/MovingText.js
+++ src/app/components/MovingText.js
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react'
-import { ReactTyped } from 'react-typed';
+
 import styles from '../css/MovingText.css'
 
 
@@ -8,14 +8,10 @@ const MovingText = () => {
   return (
     <div className = "animateType">
 
-      <ReactTyped strings={[
-          "Ask about my education",
-            "Ask about my hobbies",
-            "Ask about my projects",
-        "Ask about my community involvement",
-        "Ask when I am available"
+    
+          "Ask about my education"
             
-        ]} typeSpeed={50} backspeed={50} loop/>
+       
 
 
     </div>
