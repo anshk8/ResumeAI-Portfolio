dd8fcabd3a453ae56d38cf35c3f2e333b4b91e88
Ansh <anshkakkar05@gmail.com>
2024-11-01 12:26:55 -0400

    Added typing effect

--- src/app/components/MovingText.js
+++ src/app/components/MovingText.js
@@ -1,20 +1,21 @@
 "use client";
 import React from 'react'
-
+import Typewriter from 'typewriter-effect'; 
 import styles from '../css/MovingText.css'
 
 
 const MovingText = () => {
   return (
-    <div className = "animateType">
-
-    
-          Ask about my education
-            
-  
-
+    <div className="animateType">
+      <Typewriter options={{
+    strings: ['Ask About My Hobbies', 'Ask About My Projects', 'Ask About My Community Involvement', 'Ask About My Education'],
+    autoStart: true,
+        loop: true,
+        delay: 50,
+        deleteSpeed: 5
+  }} />
     </div>
-  )
+  );
 }
 
 export default MovingText
